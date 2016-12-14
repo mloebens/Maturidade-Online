5269ea5872db0a4aaad7d6a54ffede1d69165cba
Victor Eduardo Bittencourt <victor.eduardo@cwi.com.br>
2016-12-14 17:24:02 -0200

    feat(Infraestrutura): inserir classe de criptografia em MD5

---
+++ Maturidade-Online/Maturidade-Online.Dominio/Infraestrutura/IServicoDeCriptografia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maturidade_Online.Dominio.Infraestrutura
+{
+    public interface IServicoDeCriptografia
+    {
+
+        string Criptografar(string texto);
+
+    }
+}
---
+++ Maturidade-Online/Maturidade-Online.Infraestrutura/ServicoDeCriptografia.cs
@@ -0,0 +1,42 @@
+﻿using Maturidade_Online.Dominio.Infraestrutura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maturidade_Online.Infraestrutura
+{
+    public class ServicoDeCriptografia : IServicoDeCriptografia
+    {
+
+        public string Criptografar(string texto)
+        {
+            return ConverterParaMD5(texto);
+        }
+
+        private string ConverterParaMD5(string texto)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                // Convert the input string to a byte array and compute the hash.
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(texto));
+
+                // Create a new Stringbuilder to collect the bytes
+                // and create a string.
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data 
+                // and format each one as a hexadecimal string.
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                // Return the hexadecimal string.
+                return sBuilder.ToString();
+            }
+        }
+    }
+}
