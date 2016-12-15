e4ea6575c813246fe5f26636ed8819f0b78b3a0f
Victor Eduardo Bittencourt <victor.eduardo@cwi.com.br>
2016-12-15 17:39:32 -0200

    feat(Projeto): TO-DO View parcial - incompleto

--- Maturidade-Online/Maturidade-Online/App_Start/BundleConfig.cs
+++ Maturidade-Online/Maturidade-Online/App_Start/BundleConfig.cs
@@ -26,6 +26,12 @@ namespace Maturidade_Online
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/select2").Include(
+                        "~/Scripts/select2.js"));
+
+            bundles.Add(new StyleBundle("~/Content/select2css").Include(
+                        "~/Content/css/select2.min.css"));
+           
         }
     }
 }
