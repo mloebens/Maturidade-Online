bc39a6cdeea5f488a9c8b606c05f8efacfbb454e
Victor Eduardo Bittencourt <victor.eduardo@cwi.com.br>
2016-12-15 14:23:30 -0200

    fix(Autorizador): Redirecionar para pagina de autenticação caso não autenticado

--- Maturidade-Online/Maturidade-Online/Filter/Autorizador.cs
+++ Maturidade-Online/Maturidade-Online/Filter/Autorizador.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace Maturidade_Online.Filter
 {
@@ -18,5 +19,16 @@ namespace Maturidade_Online.Filter
 
             return true;
         }
+
+        //Redirecionar para pagina de autenticação caso não autenticado
+        protected override void HandleUnauthorizedRequest(AuthorizationContext contextoFiltro)
+        {
+            if (!contextoFiltro.HttpContext.User.Identity.IsAuthenticated)
+            {
+                contextoFiltro.Result = new RedirectToRouteResult(new
+                    RouteValueDictionary(new { controller = "Login" }));
+            }
+        }
+
     }
 }
