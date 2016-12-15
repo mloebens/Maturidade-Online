c15e64a053c79b03690378975a2434e6cd98b3a5
Unknown <maiconloebens@gmail.com>
2016-12-14 22:08:06 -0200

    feat(PilarRepositorio): implementar lógica inicial.

---
+++ Maturidade-Online/Maturidade-Online.Repositorio/Abstrato/RepositorioAbstratoEF.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Maturidade_Online.Dominio.Abstrato;
+using System.Data.Entity;
+
+namespace Maturidade_Online.Repositorio.Abstrato
+{
+    public abstract class RepositorioAbstratoEF<T> : IRepositorio<T> where T : class { 
+        
+        public void Criar(T entidade)
+        {
+            using (var contexto = new ContextoDeDadosEF())
+            {
+                contexto.Entry<T>(entidade).State = EntityState.Added;
+                contexto.SaveChanges();
+            }
+        }
+
+        public void Editar(T entidade)
+        {
+            using (var contexto = new ContextoDeDadosEF())
+            {
+                contexto.Entry<T>(entidade).State = EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+
+        public IEnumerable<T> Listar()
+        {
+            using (var contexto = new ContextoDeDadosEF())
+            {
+                return contexto.Set<T>().ToList();
+            }
+        }
+
+        public void Remover(T entidade)
+        {
+            using (var contexto = new ContextoDeDadosEF())
+            {
+                contexto.Entry<T>(entidade).State = EntityState.Deleted;
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Maturidade-Online/Maturidade-Online.Repositorio/Pilar/PilarRepositorioEF.cs
@@ -0,0 +1,14 @@
+﻿using Maturidade_Online.Dominio.Pilar;
+using Maturidade_Online.Repositorio.Abstrato;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maturidade_Online.Repositorio.Pilar
+{
+    public class PilarRepositorioEF : RepositorioAbstratoEF<PilarEntidade>
+    {
+    }
+}
