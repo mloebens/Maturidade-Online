68232ddbd884b1c93abe3e6676392cdda201a799
Unknown <maiconloebens@gmail.com>
2016-12-14 22:12:38 -0200

    feat(SubtopicoRepositorioEF): implementar lógica inicial.

---
+++ Maturidade-Online/Maturidade-Online.Repositorio/Subtopico/SubtopicoRepositorioEF.cs
@@ -0,0 +1,14 @@
+﻿using Maturidade_Online.Dominio.Subtopico;
+using Maturidade_Online.Repositorio.Abstrato;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maturidade_Online.Repositorio.Subtopico
+{
+    public class SubtopicoRepositorioEF : RepositorioAbstratoEF<SubtopicoEntidade>
+    {
+    }
+}
