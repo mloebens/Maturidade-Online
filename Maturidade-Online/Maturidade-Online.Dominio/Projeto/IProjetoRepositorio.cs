88a4744667b383f06ff450a21afd5ee9774e1634
Unknown <maiconloebens@gmail.com>
2016-12-15 01:54:55 -0200

    feat(ProjetoRepositorio): implementar lógica inicial.

---
+++ Maturidade-Online/Maturidade-Online.Dominio/Projeto/IProjetoRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using Maturidade_Online.Dominio.Abstrato;
+using Maturidade_Online.Dominio.Projeto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maturidade_Online.Dominio.Projeto
+{
+    public interface IProjetoRepositorio : IRepositorio<ProjetoEntidade>
+    {
+    }
+}
---
+++ Maturidade-Online/Maturidade-Online.Repositorio/Projeto/ProjetoRepositorioEF.cs
@@ -0,0 +1,14 @@
+﻿using Maturidade_Online.Dominio.Projeto;
+using Maturidade_Online.Repositorio.Abstrato;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maturidade_Online.Repositorio.Projeto
+{
+    public class ProjetoRepositorioEF : RepositorioAbstratoEF<ProjetoEntidade>, IProjetoRepositorio
+    {
+    }
+}
