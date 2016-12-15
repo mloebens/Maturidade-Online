6259af9a37d62686423fe9b0e1bddf15fe7acf6e
Unknown <maiconloebens@gmail.com>
2016-12-15 02:42:44 -0200

    feat(layout): implementado layout de exemplo para auxiliar desenvolvimento.

---
+++ Maturidade-Online/Maturidade-Online/Controllers/ProjetoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Maturidade_Online.Controllers
+{
+    public class ProjetoController : Controller
+    {
+        public ActionResult Novo()
+        {
+
+            return View("NovoProjeto");
+        }
+    }
+}
--- Maturidade-Online/Maturidade-Online/Global.asax.cs
+++ Maturidade-Online/Maturidade-Online/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Maturidade_Online
@@ -12,7 +13,9 @@ namespace Maturidade_Online
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
