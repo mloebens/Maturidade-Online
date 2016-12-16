6cd46dd953fa1c100fe5d1d71302cfdf411ae306
Victor <victor.eduardo@cwi.com.br>
2016-12-16 20:20:25 -0200

    feat(Projeto): inserir PartialView para Cadastro de Projetos

--- Maturidade-Online/Maturidade-Online/Controllers/ProjetoController.cs
+++ Maturidade-Online/Maturidade-Online/Controllers/ProjetoController.cs
@@ -1,8 +1,5 @@
 ﻿using AutoMapper;
 using Maturidade_Online.Dominio;
-using Maturidade_Online.Dominio;
-using Maturidade_Online.Dominio;
-using Maturidade_Online.Dominio;
 using Maturidade_Online.Filter;
 using Maturidade_Online.Models;
 using Maturidade_Online.Servicos;
@@ -18,7 +15,7 @@ namespace Maturidade_Online.Controllers
     {
         private ProjetoServico projetoServico = ServicoDeDependencia.MontarProjetoServico();
 
-        //[Autorizador]
+        [Autorizador]
         public ActionResult Manter(int? id)
         {
             var caracteristicaServico = ServicoDeDependencia.MontarCaracteristicaServico();
@@ -37,12 +34,13 @@ namespace Maturidade_Online.Controllers
                     projetoModel = Mapper.Map<Projeto, ProjetoModel>(projetoDaBase);
                 }
             }
-            projetoModel.listaDeCaracteristicas = caracteristicas;
-            projetoModel.listaDeSubtopicos = subtopicos;
+            projetoModel.ListaDeCaracteristicas = caracteristicas;
+            projetoModel.ListaDeSubtopicos = subtopicos;
 
             return View("Projeto", projetoModel);
         }
 
+        [Autorizador]
         public ActionResult Salvar(ProjetoModel projetoModel)
         {
             if (ModelState.IsValid)
@@ -60,10 +58,11 @@ namespace Maturidade_Online.Controllers
                 }
             }
 
-            return View("Projeto");
+            //return View("Projeto");
+            return RedirectToAction("Manter");
         }
 
-
+        [Autorizador]
         public ActionResult Excluir(int id)
         {
             var usuarioAutenticado = new Usuario() { Email = ServicoDeAutenticacao.UsuarioLogado.Email };
@@ -81,5 +80,21 @@ namespace Maturidade_Online.Controllers
 
             return View("Projeto");
         }
+
+
+        // Partial View
+        [Autorizador]
+        public ActionResult PesquisarSubtopicos(int[] idsCaracteristicas)
+        {
+            // TODO: consultar banco
+            var subtopicos = new[]{
+                new Subtopico() { Id = 1, Pontuacao = 5, Nome = "Subtópico 1" },
+                new Subtopico() { Id = 2, Pontuacao = 7, Nome = "Subtópico 2" }
+            };
+
+            return PartialView("_Subtopicos", subtopicos);
+        }
+
+
     }
 }
