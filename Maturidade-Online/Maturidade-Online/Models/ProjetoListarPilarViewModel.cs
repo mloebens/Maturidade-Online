560beef94a795b009a36413c84958819a252ad8c
Máicon Loebens <maiconloebens@gmail.com>
2016-12-20 20:02:31 -0200

    feat(Template): ajustar navbar.

--- Maturidade-Online/Maturidade-Online/Models/ProjetoListarPilarViewModel.cs
+++ Maturidade-Online/Maturidade-Online/Models/ProjetoListarPilarViewModel.cs
@@ -10,5 +10,21 @@ namespace Maturidade_Online.Models
         public int Id { get; set; }
         public string Titulo { get; set; }
         public decimal? Percentual { get; set; }
+
+        public string cor
+        {
+            get
+            {
+                if (this.Percentual < 26)
+                {
+                    return "progress-bar-danger";
+                }
+                if (this.Percentual >= 76)
+                {
+                    return "progress-bar-success";
+                }
+                return "progress-bar-warning";
+            }
+        }
     }
 }
